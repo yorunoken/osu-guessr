972cec8a4290b05553828bd6943ebed14fd8a22a
Yoru <m.f.fetvaci@hotmail.com>
2025-01-31 21:28:44 +0300

    trying smth

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const nextConfig: NextConfig = {
         ignoreBuildErrors: true,
     },
     reactStrictMode: false,
+    experimental: {
+        serverActions: {
+            allowedOrigins: ["guesser.yorunoken.com", "127.0.0.1:3011"],
+        },
+    },
 };
 
 export default nextConfig;
