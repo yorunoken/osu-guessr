cb31a94fa6fa6ea34ca66cad716ec339a301b124
Yoru <m.f.fetvaci@hotmail.com>
2025-02-04 05:11:17 +0300

    g

--- src/config/supporters.ts
+++ src/config/supporters.ts
@@ -5,4 +5,13 @@ export interface Supporter {
     message?: string;
 }
 
-export const supporters: Array<Supporter> = [{ name: "neriv", url: "https://guesser.yorunoken.com/user/12643934", amount: 5, message: "enjoy ur coffee bro" }];
+export const supporters: Array<Supporter> = [
+    { name: "neriv", url: "https://guesser.yorunoken.com/user/12643934", amount: 5, message: "enjoy ur coffee bro" },
+    {
+        name: "neriv",
+        url: "https://guesser.yorunoken.com/user/4573558",
+        amount: 5,
+        message: `yoru — Yesterday at 5:41 am
+    osu!guessr`,
+    },
+];
