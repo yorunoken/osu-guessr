fbc26c8b065bef0a18390d49735c351b1b8bd47c
Yoru <m.f.fetvaci@hotmail.com>
2025-02-04 05:27:11 +0300

    g

--- src/config/supporters.ts
+++ src/config/supporters.ts
@@ -8,7 +8,7 @@ export interface Supporter {
 export const supporters: Array<Supporter> = [
     { name: "neriv", url: "https://guesser.yorunoken.com/user/12643934", amount: 5, message: "enjoy ur coffee bro" },
     {
-        name: "neriv",
+        name: "Eric",
         url: "https://guesser.yorunoken.com/user/4573558",
         amount: 5,
         message: `yoru — Yesterday at 5:41 am
