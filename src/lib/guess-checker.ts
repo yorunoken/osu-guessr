4bcd9cdfa61581d7e870d8af2da3c3960f29583e
Yoru <m.f.fetvaci@hotmail.com>
2025-02-04 23:05:29 +0300

    Fix string detection

--- src/lib/guess-checker.ts
+++ src/lib/guess-checker.ts
@@ -6,10 +6,34 @@ export enum GuessDifficulty {
     Impossible = 0.95,
 }
 
-export function checkGuess(guess: string, actual: string, difficulty: GuessDifficulty = 0.5): boolean {
-    console.log({ guess, actual, difficulty });
-    const normalizeString = (str: string) => str.toLowerCase().replace(/[^a-z0-9\s]/g, "");
+function normalizeString(str: string): string {
+    let normalized = str.toLowerCase().replace(/[^a-z0-9\s]/g, "");
 
+    const openParenIndex = str.indexOf("(");
+    const closeParenIndex = str.lastIndexOf(")");
+
+    if (openParenIndex !== -1 && closeParenIndex !== -1) {
+        normalized = str.slice(0, openParenIndex) + str.slice(closeParenIndex + 1);
+    }
+
+    const featIndex = normalized.indexOf("feat");
+    const ftIndex = normalized.indexOf("ft");
+
+    if (featIndex !== -1) {
+        normalized = normalized.slice(0, featIndex);
+    } else if (ftIndex !== -1) {
+        normalized = normalized.slice(0, ftIndex);
+    }
+
+    normalized = normalized
+        .toLowerCase()
+        .replace(/[^a-z0-9\s]/g, "")
+        .trim();
+
+    return normalized;
+}
+
+export function checkGuess(guess: string, actual: string, difficulty: GuessDifficulty = 0.5): boolean {
     guess = normalizeString(guess);
     actual = normalizeString(actual);
 
