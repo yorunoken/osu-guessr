e7c5876eef50f3489bf3fc1f51aeee593dc631c7
Yoru <m.f.fetvaci@hotmail.com>
2025-02-07 01:34:31 +0300

    updated footer, added privacy part

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+import PrivacyPolicy from "./client";
+
+export const metadata: Metadata = {
+    title: "Privacy Policy | osu!guessr",
+    description: "Learn about how we handle your data and privacy on osu!guessr.",
+};
+
+export default function PrivacyPolicyPage() {
+    return <PrivacyPolicy />;
+}
