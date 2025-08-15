db1db68ed3974929b7020e21358b268b323f6357
Yoru <me@yorunoken.com>
2025-08-15 14:48:31 +0300

    fallback

--- src/lib/database/database.ts
+++ src/lib/database/database.ts
@@ -10,7 +10,7 @@ const DEFAULT_OPTIONS: Required<QueryOptions> = {
 };
 
 const connection = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "127.0.0.1",
     port: Number(process.env.DB_PORT) || 3306,
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "",
